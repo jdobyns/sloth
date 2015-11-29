9b4b83307d017e62fecedd474f47303961d4aebe
James Loosli <loosli@gmail.com>
2015-11-29 14:01:52 -0800

    the one that seems to know about multiple databases

--- sloth/routes/index.js
+++ sloth/routes/index.js
@@ -3,9 +3,21 @@ var router = express.Router();
 var os = require('os');
 var MongoClient = require('mongodb').MongoClient,
   test = require('assert');
-// Connection url
+var fs = require('fs')// Connection url
+var url = fs.readFile('/etc/hosts', 'utf8', function (err, data) {
+    if (err) console.log(err);
+    var array = data.toString().split("\n",30);
+    var goodIps = []
+    for (var i = 0, len = array.length; i < len; i++) {
+      if (array[i].indexOf("_mongo_") >= 0){
+        ip = array[i].split(/\s+/)[0];
+        goodIps.push(ip);
+      }
+    }
+    return "mongodb://" + goodIps.join(",") + "/log";
+});
 //var url = 'mongodb://mongo:27017/log';
-var url = 'mongodb://localhost:27017/log';
+//var url = 'mongodb://localhost:27017/log';
 var db = 'log';
 
 // Connect using MongoClient
@@ -17,8 +29,8 @@ var incrementVisit = function(db, cb) {
         if (err) console.log(err);
         collection.findOne({counter: "visit counter"},{fields: {totalVisitCount: 1}}, function(err, res) {
           if (err) console.log(err);
-          return cb(null, res.totalVisitCount);
           db.close();
+          return cb(null, res.totalVisitCount);
         });
       });
     });
