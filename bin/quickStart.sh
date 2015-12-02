f9845d9b380933f115195717c3288a99f163563c
James Loosli <loosli@gmail.com>
2015-12-02 01:44:43 -0800

    the one where I realize it's 2am

--- bin/quickStart.sh
+++ bin/quickStart.sh
@@ -8,6 +8,10 @@
 # Set up the docker host. This will be an ubuntu 14.04 image according to the docker-machine docs
 docker-machine create --driver amazonec2 aws01
 
-
+# Set up cloudflare with the right IP address
+curl -X GET "https://api.cloudflare.com/client/v4/zones/cd7d0123e3012345da9420df9514dad0" \
+  -H "Content-Type:application/json" \
+  -H "X-Auth-Key:1234567893feefc5f0q5000bfo0c38d90bbeb" \
+  -H "X-Auth-Email:example@example.com"
 
 
