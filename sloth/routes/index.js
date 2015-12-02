afd4175224a02e0134d82b58e16e2c922e7a16d7
James Loosli <loosli@gmail.com>
2015-12-01 23:47:18 -0800

    cleaning up the nodejs app

--- sloth/routes/index.js
+++ sloth/routes/index.js
@@ -3,25 +3,10 @@ var router = express.Router();
 var os = require('os');
 var MongoClient = require('mongodb').MongoClient,
   test = require('assert');
-var fs = require('fs')// Connection url
-var url = fs.readFile('/etc/hosts', 'utf8', function (err, data) {
-    if (err) console.log(err);
-    var array = data.toString().split("\n",30);
-    var goodIps = []
-    for (var i = 0, len = array.length; i < len; i++) {
-      if (array[i].indexOf("_mongo_") >= 0){
-        ip = array[i].split(/\s+/)[0];
-        goodIps.push(ip);
-      }
-    }
-    return "mongodb://" + goodIps.join(",") + "/log";
-});
-//var url = 'mongodb://mongo:27017/log';
-//var url = 'mongodb://localhost:27017/log';
-var db = 'log';
+var url = "mongodb://mongo:27017/log";
 
 // Connect using MongoClient
-var incrementVisit = function(db, cb) {
+var incrementVisit = function(url, cb) {
   MongoClient.connect(url, function(err, db) {
     if (err) console.log(err);
     db.collection('log', function(err, collection) {
@@ -39,8 +24,10 @@ var incrementVisit = function(db, cb) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  incrementVisit(db, function(err, visitCount) {
-    res.render('index', { title: 'Sloth!', hostname: os.hostname(), visit: visitCount });
+  getMongoUrl(url, function(err, url) {
+    incrementVisit(url, function(err, visitCount) {
+      res.render('index', { title: 'Sloth!', hostname: os.hostname(), visit: visitCount});
+    });
   });
 });
 
