ce51207be14137421792928f0eefc29b8749e360
James Loosli <loosli@gmail.com>
2015-12-01 23:49:00 -0800

    shell scripting this stuff out

---
+++ bin/quickStart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#This is designed around AWS, but could easily be adapted to a local boot2docker
+# Make sure the following are defined in your env;
+# - AWS_ACCESS_KEY_ID
+# - AWS_SECRET_ACCESS_KEY
+# - AWS_VPC_ID
+
+# Set up the docker host. This will be an ubuntu 14.04 image according to the docker-machine docs
+docker-machine create --driver amazonec2 aws01
+
+
+
+
